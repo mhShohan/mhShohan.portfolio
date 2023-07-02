850b5777c74f0f56556f65a9ee26d98dd92a4f63
mhshohan1 <mehdihasanshohan25@gmail.com>
2023-07-02 22:23:26 +0600

    update banner an education timeline

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,40 +5,45 @@ import Skill from './Skill';
 import { Heading } from './ReuseableComponents';
 import { skills } from '../data';
 import Fade from 'react-reveal/Fade';
+import Education from './Education';
 
 export default function About() {
-  return (
-    <AboutSection id="AboutSection">
-      <Container maxWidth="lg">
-        <Fade bottom>
-          <Heading>About</Heading>
-        </Fade>
-        <br />
-        <br />
-        <Grid container>
-          <Grid item md={5}>
-            <Fade left>
-              <Description>
-                Hi, I'm Mehdi Hasan Shohan, a passionate self-taught programmer.
-                I'm learning from the open-source community and trying to
-                contribute to the open source community. I started my
-                programming journey about 2 years ago. I am very curious about
-                web technologies and I'm learning several technologies and
-                comfortable with working with these technologies. Currently, I
-                am working with MERN Stack development and practicing on
-                personal projects. I'm comfortable in Javascript, Node.js,
-                Express.js, RestAPIs, JWT, React, Context API, Bootstrap,
-                Material UI, Styled Components, SASS, etc...!!!
-              </Description>
-              <br />
-              <Description>
-                <span>Interested in:</span> Softwere Engineering, Microservices,
-                AWS, Web 3.0 ...!!!
-              </Description>
-            </Fade>
-          </Grid>
-          <Grid item md={7}>
-            <Fade right>
+    return (
+        <AboutSection id='AboutSection'>
+            <Container maxWidth='lg'>
+                <Fade bottom>
+                    <Heading>About</Heading>
+                </Fade>
+                <br />
+                <br />
+                <Grid container>
+                    <Grid item md={12}>
+                        <Fade left>
+                            <Description>
+                                Hi, I'm Mehdi Hasan Shohan, a passionate
+                                self-taught programmer. I'm learning from the
+                                open-source community and trying to contribute
+                                to the open source community. I started my
+                                programming journey about 3 years ago. I am very
+                                curious about web technologies and I'm learning
+                                several technologies and comfortable with
+                                working with these technologies. Currently, I am
+                                working with MERN Stack development and
+                                practicing on personal projects. I'm comfortable
+                                in Javascript, Node.js, Express.js, RestAPIs,
+                                JWT, React, Context API, Bootstrap, Material UI,
+                                Styled Components, SASS, etc...!!!
+                            </Description>
+                            <br />
+                            <Description>
+                                <span>Interested in:</span> Softwere
+                                Engineering, Microservices, AWS, Web 3.0 ...!!!
+                            </Description>
+                        </Fade>
+                    </Grid>
+                    <Grid item md={12}>
+                        <Education />
+                        {/* <Fade right>
               <SkillsSection>
                 <h2>Tools And Technologies I'm using</h2>
                 <Grid container>
@@ -93,103 +98,103 @@ export default function About() {
                   </Grid>
                 </Grid>
               </SkillsSection>
-            </Fade>
-          </Grid>
-        </Grid>
-      </Container>
-    </AboutSection>
-  );
+            </Fade> */}
+                    </Grid>
+                </Grid>
+            </Container>
+        </AboutSection>
+    );
 }
 
 const AboutSection = styled.section`
-  background: rgba(71, 109, 124, 0.5);
-  padding: 70px 0 110px 0;
+    background: rgba(71, 109, 124, 0.5);
+    padding: 70px 0 110px 0;
 
-  h3 {
-    font-size: 26px;
-    font-weight: 600;
-    font-family: 'Roboto', sans-serif;
-    line-height: 1;
-  }
+    h3 {
+        font-size: 26px;
+        font-weight: 600;
+        font-family: 'Roboto', sans-serif;
+        line-height: 1;
+    }
 `;
 
 const Description = styled.p`
-  font-size: 18px;
-  font-family: 'Roboto', sans-serif;
-  font-weight: 300;
-  text-align: justify;
-  margin: 0 30px;
-  letter-spacing: -0.9px;
-  line-height: 24px;
+    font-size: 18px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    text-align: justify;
+    margin: 0 30px;
+    letter-spacing: -0.9px;
+    line-height: 24px;
 
-  span {
-    font-weight: 600;
-    text-decoration: underline;
-  }
+    span {
+        font-weight: 600;
+        text-decoration: underline;
+    }
 
-  @media (max-width: 600px) {
-    margin: 0;
-    padding: 0 5px;
-  }
+    @media (max-width: 600px) {
+        margin: 0;
+        padding: 0 5px;
+    }
 `;
 
 const SkillsSection = styled.div`
-  h2 {
-    font-family: 'Roboto', sans-serif;
-    text-align: center;
-    font-size: 2rem;
-    text-transform: capitalize;
-    margin-bottom: 20px;
-
-    &::after {
-      content: '';
-      display: block;
-      width: 40%;
-      height: 2px;
-      background: #000;
-      margin: 0.3rem auto;
-    }
-  }
-  @media (max-width: 1200px) {
-    h3 {
-      font-size: 1.4rem;
-    }
-  }
-  @media (max-width: 1000px) {
-    h3 {
-      font-size: 1.2rem;
-    }
-  }
-  @media (max-width: 900px) {
-    h3 {
-      font-size: 1.6rem;
-    }
-  }
-  @media (max-width: 700px) {
-    h3 {
-      font-size: 1.4rem;
-    }
-  }
-
-  @media (max-width: 600px) {
-    h3 {
-      text-align: center;
-      margin-bottom: 6px;
-    }
     h2 {
-      margin-top: 20px;
-      font-size: 1.8rem;
+        font-family: 'Roboto', sans-serif;
+        text-align: center;
+        font-size: 2rem;
+        text-transform: capitalize;
+        margin-bottom: 20px;
+
+        &::after {
+            content: '';
+            display: block;
+            width: 40%;
+            height: 2px;
+            background: #000;
+            margin: 0.3rem auto;
+        }
     }
-  }
-  @media (max-width: 450px) {
-    h2 {
-      font-size: 1.5rem;
-      margin-top: 20px;
+    @media (max-width: 1200px) {
+        h3 {
+            font-size: 1.4rem;
+        }
     }
-  }
+    @media (max-width: 1000px) {
+        h3 {
+            font-size: 1.2rem;
+        }
+    }
+    @media (max-width: 900px) {
+        h3 {
+            font-size: 1.6rem;
+        }
+    }
+    @media (max-width: 700px) {
+        h3 {
+            font-size: 1.4rem;
+        }
+    }
+
+    @media (max-width: 600px) {
+        h3 {
+            text-align: center;
+            margin-bottom: 6px;
+        }
+        h2 {
+            margin-top: 20px;
+            font-size: 1.8rem;
+        }
+    }
+    @media (max-width: 450px) {
+        h2 {
+            font-size: 1.5rem;
+            margin-top: 20px;
+        }
+    }
 `;
 
 const SkillsWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+    display: flex;
+    flex-wrap: wrap;
 `;
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,192 +6,197 @@ import { LinkItem } from './ReuseableComponents';
 import Fade from 'react-reveal/Fade';
 
 export default function Banner() {
-  return (
-    <BannerWrapper>
-      <BannerLeft>
-        <img src="./images/shohan.png" alt="shohan" />
-      </BannerLeft>
-      <BannerRight>
-        <Fade bottom>
-          <div className="centerElements">
-            <img src={mobileImg} alt="shohan" className="mobileImg" />
-            <Name>Mehdi Hasan Shohan</Name>
-            <Title>
-              Web Developer Enthusiast ||&nbsp;
-              {/* <> Programer</> */}
-              <Typer
-                heading="Things for hipsters:"
-                dataText={['Programmer', 'Learner', 'Developer']}
-              />
-            </Title>
-            <Decription>
-              An enthusiast student who loves to learn and explore the world of
-              web development.
-            </Decription>
-            <LinkItem banner={true} />
-            <DownLoadButton
-              href="https://drive.google.com/file/d/1aRiVz-q9f8rywx3tjNCVk_JKaKx3Suhx/view?usp=sharing"
-              rel="noreferrer"
-              target="_blank"
-            >
-              Resume
-            </DownLoadButton>
-            <br />
-            <br />
-          </div>
-        </Fade>
-      </BannerRight>
-    </BannerWrapper>
-  );
+    return (
+        <BannerWrapper>
+            <BannerLeft>
+                <img src={mobileImg} alt='shohan' />
+            </BannerLeft>
+            <BannerRight>
+                <Fade bottom>
+                    <div className='centerElements'>
+                        <img
+                            src={mobileImg}
+                            alt='shohan'
+                            className='mobileImg'
+                        />
+                        <Name>Mehdi Hasan Shohan</Name>
+                        <Title>
+                            Junior MERN Stack Developer ||&nbsp;
+                            {/* <> Programer</> */}
+                            <Typer
+                                heading='Things for hipsters:'
+                                dataText={[
+                                    'Programmer',
+                                    'Learner',
+                                    'Developer',
+                                ]}
+                            />
+                        </Title>
+                        <Description>
+                            An enthusiast student who loves to learn and explore
+                            the world of web development.
+                        </Description>
+                        <LinkItem banner={true} />
+                        <DownLoadButton
+                            href='https://drive.google.com/file/d/1aRiVz-q9f8rywx3tjNCVk_JKaKx3Suhx/view?usp=sharing'
+                            rel='noreferrer'
+                            target='_blank'
+                        >
+                            Resume
+                        </DownLoadButton>
+                        <br />
+                        <br />
+                    </div>
+                </Fade>
+            </BannerRight>
+        </BannerWrapper>
+    );
 }
 
 const BannerWrapper = styled.section`
-  margin: 0 2rem;
-  display: flex;
-  min-height: 80vh;
+    margin: 2rem;
+    display: flex;
+    min-height: 80vh;
 `;
 
 const BannerLeft = styled.div`
-  flex: 1;
-  img {
-    margin-top: 80px;
-    width: 90%;
-    padding: 0 50px;
-    margin-bottom: -3px;
+    flex: 1.5;
+    img {
+        width: 100%;
+        padding: 0 50px;
 
-    @media (max-width: 1300px) {
-      width: 90%;
+        @media (max-width: 1300px) {
+            width: 100%;
+        }
     }
-  }
-  @media (max-width: 1360px) {
-    flex: 1.2;
-  }
-  @media (max-width: 1250px) {
-    flex: 1.2;
-  }
-  @media (max-width: 1150px) {
-    flex: 1.4;
-  }
-  @media (max-width: 1050px) {
-    flex: 1.6;
-  }
-  @media (max-width: 1050px) {
-    flex: 1.8;
-  }
-  @media (max-width: 900px) {
-    flex: 2;
-  }
-  @media (max-width: 900px) {
-    display: none;
-  }
+    @media (max-width: 1360px) {
+        flex: 1.2;
+    }
+    @media (max-width: 1250px) {
+        flex: 1.2;
+    }
+    @media (max-width: 1150px) {
+        flex: 1.4;
+    }
+    @media (max-width: 1050px) {
+        flex: 1.6;
+    }
+    @media (max-width: 1050px) {
+        flex: 1.8;
+    }
+    @media (max-width: 900px) {
+        flex: 2;
+    }
+    @media (max-width: 900px) {
+        display: none;
+    }
 `;
 const BannerRight = styled.div`
-  flex: 2;
-  display: flex;
-  align-items: center;
-
-  @media (max-width: 900px) {
-    .centerElements {
-      display: flex;
-      align-items: center;
-      flex-direction: column;
-      margin-bottom: 0px;
-    }
-  }
-
-  .mobileImg {
-    display: none;
+    flex: 2;
+    display: flex;
+    align-items: center;
 
     @media (max-width: 900px) {
-      display: block;
-      width: 90%;
-      margin: 80px 0 0 0;
+        .centerElements {
+            display: flex;
+            align-items: center;
+            flex-direction: column;
+            margin-bottom: 0px;
+        }
     }
-  }
+
+    .mobileImg {
+        display: none;
+
+        @media (max-width: 900px) {
+            display: block;
+            width: 100%;
+        }
+    }
 `;
 
 const Name = styled.h1`
-  color: #142850;
-  font-family: 'Balsamiq Sans', cursive;
-  font-size: 5rem;
-  letter-spacing: -3px;
-
-  @media (max-width: 1200px) {
+    color: #142850;
+    font-family: 'Balsamiq Sans', cursive;
     font-size: 4rem;
-  }
-  @media (max-width: 1100px) {
-    font-size: 3rem;
-  }
-  @media (max-width: 900px) {
-    font-size: 4.4rem;
-    letter-spacing: -1px;
-  }
-  @media (max-width: 750px) {
-    font-size: 3.6rem;
-  }
-  @media (max-width: 700px) {
-    font-size: 2.6rem;
-  }
-  @media (max-width: 500px) {
-    font-size: 2.2rem;
-  }
+    letter-spacing: -3px;
+
+    @media (max-width: 1200px) {
+        font-size: 4rem;
+    }
+    @media (max-width: 1100px) {
+        font-size: 3rem;
+    }
+    @media (max-width: 900px) {
+        font-size: 4.4rem;
+        letter-spacing: -1px;
+    }
+    @media (max-width: 750px) {
+        font-size: 3.6rem;
+    }
+    @media (max-width: 700px) {
+        font-size: 2.6rem;
+    }
+    @media (max-width: 500px) {
+        font-size: 2.2rem;
+    }
 `;
 const Title = styled.h1`
-  color: #142850;
-  display: flex;
-  font-family: 'Balsamiq Sans', cursive;
-  letter-spacing: -2px;
-  font-size: 2.4rem;
-  padding-right: 10px;
+    color: #142850;
+    display: flex;
+    font-family: 'Balsamiq Sans', cursive;
+    letter-spacing: -2px;
+    font-size: 2rem;
+    padding-right: 10px;
 
-  @media (max-width: 1200px) {
-    font-size: 2rem;
-  }
-  @media (max-width: 1100px) {
-    font-size: 1.4rem;
-  }
-  @media (max-width: 900px) {
-    font-size: 2rem;
-  }
-  @media (max-width: 700px) {
-    font-size: 1.4rem;
-  }
-  @media (max-width: 500px) {
-    font-size: 1.1rem;
-    letter-spacing: -1px;
-  }
+    @media (max-width: 1200px) {
+        font-size: 2rem;
+    }
+    @media (max-width: 1100px) {
+        font-size: 1.4rem;
+    }
+    @media (max-width: 900px) {
+        font-size: 2rem;
+    }
+    @media (max-width: 700px) {
+        font-size: 1.4rem;
+    }
+    @media (max-width: 500px) {
+        font-size: 1.1rem;
+        letter-spacing: -1px;
+    }
 `;
 
-const Decription = styled.p`
-  font-size: 24px;
-  color: #142850;
-  /* margin-bottom: 10px; */
-  margin-right: 30px;
+const Description = styled.p`
+    font-size: 24px;
+    color: #142850;
+    /* margin-bottom: 10px; */
+    margin-right: 30px;
 
-  @media (max-width: 1200px) {
-    font-size: 20px;
-  }
-  @media (max-width: 900px) {
-    text-align: center;
-    font-size: 18px;
-    margin-bottom: 0px;
-    margin-right: 0px;
-  }
+    @media (max-width: 1200px) {
+        font-size: 20px;
+    }
+    @media (max-width: 900px) {
+        text-align: center;
+        font-size: 18px;
+        margin-bottom: 0px;
+        margin-right: 0px;
+    }
 `;
 
 const DownLoadButton = styled.a`
-  color: #142850;
-  font-size: 26px;
-  font-weight: 500;
-  padding: 6px 20px;
-  border: 1px solid #142850;
-  border-radius: 2px;
-  cursor: pointer;
+    color: #142850;
+    font-size: 26px;
+    font-weight: 500;
+    padding: 6px 20px;
+    border: 1px solid #142850;
+    border-radius: 2px;
+    cursor: pointer;
 
-  transition: all ease 300ms;
+    transition: all ease 300ms;
 
-  &:hover {
-    background: #142850;
-    color: #fff;
-  }
+    &:hover {
+        background: #142850;
+        color: #fff;
+    }
 `;
